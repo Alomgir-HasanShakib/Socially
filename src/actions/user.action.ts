60e285dc2b252b6d05087eacdd0b287441c54a71
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2025-01-22 23:19:56 +0600

    created user action to create user data for saving to the database

---
+++ src/actions/user.action.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+export const syncUser = async () => {
+  try {
+    const { userId } = await auth();
+    const user = await currentUser();
+    if (!userId || !user) return;
+    const dbUser = await prisma.user.create({
+      data: {
+        clerckId: userId,
+        name: `${user.firstName || ""} ${user.lastName || ""}`,
+        username:
+          user.username ?? user.emailAddresses[0].emailAddress.split("@")[0],
+        email: user.emailAddresses[0].emailAddress,
+        image: user.imageUrl,
+      },
+    });
+    return dbUser;
+  } catch (error) {
+    console.log("Error in syncUser", error);
+  }
+};
