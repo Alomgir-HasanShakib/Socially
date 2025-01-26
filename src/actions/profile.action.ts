9af58e66b19dd564dd2b3200561bd71b9c46f814
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2025-01-26 19:16:20 +0600

    created function for update profile and showing folloeing user

--- src/actions/profile.action.ts
+++ src/actions/profile.action.ts
@@ -88,3 +88,106 @@ export const getUserPosts = async (userId: string) => {
     throw new Error("Failed to fetch posts");
   }
 };
+
+export const getUserLikedPosts = async (userId: string) => {
+  try {
+    const likedPosts = await prisma.post.findMany({
+      where: {
+        likes: {
+          some: {
+            userId,
+          },
+        },
+      },
+      include: {
+        author: {
+          select: {
+            id: true,
+            name: true,
+            username: true,
+            image: true,
+          },
+        },
+        comments: {
+          include: {
+            author: {
+              select: {
+                id: true,
+                name: true,
+                username: true,
+                image: true,
+              },
+            },
+          },
+          orderBy: {
+            createdAt: "asc",
+          },
+        },
+        likes: {
+          select: {
+            userId: true,
+          },
+        },
+        _count: {
+          select: {
+            comments: true,
+            likes: true,
+          },
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return likedPosts;
+  } catch (error) {
+    throw new Error("Failed to fetch user liked post");
+  }
+};
+
+export const updateProfile = async (formData: FormData) => {
+  try {
+    const { userId: clerckId } = await auth();
+    if (!clerckId) throw new Error("Unauthorized");
+    const name = formData.get("name") as string;
+    const bio = formData.get("bio") as string;
+    const location = formData.get("location") as string;
+    const website = formData.get("website") as string;
+
+    const user = await prisma.user.update({
+      where: {
+        clerckId,
+      },
+      data: {
+        name,
+        bio,
+        location,
+        website,
+      },
+    });
+    revalidatePath(`/profile`);
+    return user;
+  } catch (error) {
+    throw new Error("Failed to update profile");
+  }
+};
+
+export const isFollowing = async (userId: string) => {
+  const { userId: currentUserId } = await auth();
+  if (!currentUserId) return false;
+
+  try {
+    const follow = await prisma.follows.findUnique({
+      where: {
+        followerId_followingId: {
+          followerId: currentUserId,
+          followingId: userId,
+        },
+      },
+    });
+    return !!follow;
+  } catch (error) {
+    throw new Error("Failed to check follow status");
+    return false;
+  }
+};
