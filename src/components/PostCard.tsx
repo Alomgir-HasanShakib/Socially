64ffb0b0d29afc61f3c5e9295b27d7ae6d7d2a2a
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2025-01-26 17:01:33 +0600

    handle like, comment and delete functionality into the client end

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,46 +1,84 @@
 "use client";
-import { getPosts, toggleLike } from '@/actions/post.action';
-import { useUser } from '@clerk/nextjs';
-import { useState } from 'react';
+import {
+  createComment,
+  deletePost,
+  getPosts,
+  toggleLike,
+} from "@/actions/post.action";
+import { useUser } from "@clerk/nextjs";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
-type Posts = Awaited<ReturnType<typeof getPosts>>
+type Posts = Awaited<ReturnType<typeof getPosts>>;
 type Post = NonNullable<Posts>[number];
 
-const PostCard = ({ post, dbUserId }: { post: Post; dbUserId: string | null }) => {
-    const { user } = useUser()
-    const [newComment, setNewComment] = useState('')
-    const [isCommenting, setIsCommenting] = useState(false)
-    const [isLiking, setIsLiking] = useState(false)
-    const [isDeleting, setIsDeleting] = useState(false)
-    const [hasLiked, setHasLiked] = useState(post.likes.some(like => like.userId === dbUserId))
-    const [optimisticLikes, setOptimisticLikes] = useState(post._count.likes)
+const PostCard = ({
+  post,
+  dbUserId,
+}: {
+  post: Post;
+  dbUserId: string | null;
+}) => {
+  const { user } = useUser();
+  const [newComment, setNewComment] = useState("");
+  const [isCommenting, setIsCommenting] = useState(false);
+  const [isLiking, setIsLiking] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [hasLiked, setHasLiked] = useState(
+    post.likes.some((like) => like.userId === dbUserId)
+  );
+  const [optimisticLikes, setOptimisticLikes] = useState(post._count.likes);
 
-    const handleLike = async () => {
-        if (isLiking) return
-        try {
-            setIsLiking(true)
-            setHasLiked(prev => !prev)
-            setOptimisticLikes(prev => prev + (hasLiked ? -1 : 1))
+  const handleLike = async () => {
+    if (isLiking) return;
+    try {
+      setIsLiking(true);
+      setHasLiked((prev) => !prev);
+      setOptimisticLikes((prev) => prev + (hasLiked ? -1 : 1));
 
-            await toggleLike(post.id)
-
-        } catch (error) {
-            console.log('Failed to like post:', error)
-            setOptimisticLikes(post._count.likes)
-            setHasLiked(post.likes.some(like => like.userId === dbUserId))
-
-        } finally {
-            setIsLiking(false)
-        }
+      await toggleLike(post.id);
+    } catch (error) {
+      setOptimisticLikes(post._count.likes);
+      setHasLiked(post.likes.some((like) => like.userId === dbUserId));
+    } finally {
+      setIsLiking(false);
     }
+  };
 
-    const handleAddComment = async () => { }
+  const handleAddComment = async () => {
+    if (!newComment.trim() || isCommenting) return;
+    try {
+      setIsCommenting(true);
+      const res = await createComment(post.id, newComment);
+      if (res?.success) {
+        toast.success("Comment added successfully");
+        setNewComment("");
+      }
+      // await addComment(post.id, newComment)
+    } catch (error) {
+      toast.error("Failed to add comment");
+    } finally {
+      setIsCommenting(false);
+    }
+  };
 
-    const handleDelete = async () => { }
+  const handleDelete = async () => {
+    if (isDeleting) return;
+    try {
+      setIsDeleting(true);
+      const res = await deletePost(post.id);
+      if (res?.success) {
+        toast.success("Post deleted successfully");
+      }
+      // await deletePost(post.id);
+    } catch (error) {
+      toast.error("Failed to delete post");
+    } finally {
+      setIsDeleting(false);
+    }
+  };
 
-    return (
-        <div>PostCard</div>
-    )
-}
+  return <div>PostCard</div>;
+};
 
-export default PostCard
+export default PostCard;
